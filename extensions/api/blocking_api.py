6f9b7bae0918cf8ea0f711523e364e277602de72
TruthSearchers <40608534+TruthSearchers@users.noreply.github.com>
2023-05-31 09:05:47 +0530

    Update blocking_api.py

--- extensions/api/blocking_api.py
+++ extensions/api/blocking_api.py
@@ -5,7 +5,8 @@ from threading import Thread
 from extensions.api.util import build_parameters, try_start_cloudflared
 from modules import shared
 from modules.chat import generate_chat_reply
-from modules.text_generation import encode, generate_reply
+from modules.text_generation import encode, generate_reply, stop_everything_event
+
 
 
 class Handler(BaseHTTPRequestHandler):
@@ -78,6 +79,19 @@ class Handler(BaseHTTPRequestHandler):
 
             self.wfile.write(response.encode('utf-8'))
 
+        elif self.path == '/api/v1/stop-stream':
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+
+            stop_everything_event()
+
+            response = json.dumps({
+                'results': 'success'
+            })
+
+            self.wfile.write(response.encode('utf-8')
+                             
         elif self.path == '/api/v1/token-count':
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
