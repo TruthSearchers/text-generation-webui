fa44b39843a94e9e0f47ae56034668f00547cf30
TruthSearchers <40608534+TruthSearchers@users.noreply.github.com>
2023-06-15 09:32:49 +0530

    Update api-example.py

--- api-examples/api-example.py
+++ api-examples/api-example.py
@@ -12,8 +12,11 @@ def run(prompt):
     request = {
         'prompt': prompt,
         'max_new_tokens': 250,
+        # Generation params. If 'preset' is set to different than 'None', the values
+        # in presets/preset-name.yaml are used instead of the individual numbers.
+        'preset': 'None',
         'do_sample': True,
-        'temperature': 1.3,
+        'temperature': 0.7,
         'top_p': 0.1,
         'typical_p': 1,
         'epsilon_cutoff': 0,  # In units of 1e-4
