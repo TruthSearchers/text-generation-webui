0145570d6bffc82766c6461c4e1a6eaca98ef7c8
TruthSearchers <40608534+TruthSearchers@users.noreply.github.com>
2023-06-17 11:57:32 +0530

    Update server.py

--- server.py
+++ server.py
@@ -606,6 +606,9 @@ def create_interface():
             with gr.Tab("⚙️ Settings Station", elem_id="parameters"):
                 
                 create_settings_menus(default_preset)
+                shared.gradio['toggle_dark_mode'] = gr.Button('Dark/Light ☾ mode', elem_classes="small-button")
+                shared.gradio['toggle_dark_mode'].click(lambda: None, None, None, _js='() => {document.getElementsByTagName("body")[0].classList.toggle("dark")}')
+
 
         # Create notebook mode interface
         elif shared.args.notebook:
@@ -682,52 +685,52 @@ def create_interface():
                 create_settings_menus(default_preset)
 
         # Model tab
-        with gr.Tab("Model", elem_id="model-tab"):
+        with gr.Tab("🧠 Model", elem_id="model-tab"):
             create_model_menus()
 
         # Training tab
         #with gr.Tab("Training", elem_id="training-tab"):
             #training.create_train_interface()
 
-        # Interface mode tab
-        with gr.Tab("Interface mode", elem_id="interface-mode"):
-            modes = ["default", "notebook", "chat"]
-            current_mode = "default"
-            for mode in modes[1:]:
-                if getattr(shared.args, mode):
-                    current_mode = mode
-                    break
+#         # Interface mode tab
+#         with gr.Tab("Interface mode", elem_id="interface-mode"):
+#             modes = ["default", "notebook", "chat"]
+#             current_mode = "default"
+#             for mode in modes[1:]:
+#                 if getattr(shared.args, mode):
+#                     current_mode = mode
+#                     break
 
-            cmd_list = vars(shared.args)
-            bool_list = sorted([k for k in cmd_list if type(cmd_list[k]) is bool and k not in modes + ui.list_model_elements()])
-            bool_active = [k for k in bool_list if vars(shared.args)[k]]
+#             cmd_list = vars(shared.args)
+#             bool_list = sorted([k for k in cmd_list if type(cmd_list[k]) is bool and k not in modes + ui.list_model_elements()])
+#             bool_active = [k for k in bool_list if vars(shared.args)[k]]
 
-            with gr.Row():
-                shared.gradio['interface_modes_menu'] = gr.Dropdown(choices=modes, value=current_mode, label="Mode")
-                shared.gradio['toggle_dark_mode'] = gr.Button('Toggle dark/light mode', elem_classes="small-button")
+#             with gr.Row():
+#                 shared.gradio['interface_modes_menu'] = gr.Dropdown(choices=modes, value=current_mode, label="Mode")
+#                 shared.gradio['toggle_dark_mode'] = gr.Button('Toggle dark/light mode', elem_classes="small-button")
 
-            with gr.Row():
-                with gr.Column():
-                    shared.gradio['extensions_menu'] = gr.CheckboxGroup(choices=utils.get_available_extensions(), value=shared.args.extensions, label="Available extensions", info='Note that some of these extensions may require manually installing Python requirements through the command: pip install -r extensions/extension_name/requirements.txt', elem_classes='checkboxgroup-table')
-                with gr.Column():
-                    shared.gradio['bool_menu'] = gr.CheckboxGroup(choices=bool_list, value=bool_active, label="Boolean command-line flags", elem_classes='checkboxgroup-table')
-            shared.gradio['reset_interface'] = gr.Button("Apply and restart the interface")
-            with gr.Row():
-                extension_name = gr.Textbox(lines=1, label='Install or update an extension', info='Enter the GitHub URL below. For a list of extensions, see: https://github.com/oobabooga/text-generation-webui-extensions ⚠️  WARNING ⚠️ : extensions can execute arbitrary code. Make sure to inspect their source code before activating them.')
-                extension_install = gr.Button('Install or update', elem_classes="small-button")
+#             with gr.Row():
+#                 with gr.Column():
+#                     shared.gradio['extensions_menu'] = gr.CheckboxGroup(choices=utils.get_available_extensions(), value=shared.args.extensions, label="Available extensions", info='Note that some of these extensions may require manually installing Python requirements through the command: pip install -r extensions/extension_name/requirements.txt', elem_classes='checkboxgroup-table')
+#                 with gr.Column():
+#                     shared.gradio['bool_menu'] = gr.CheckboxGroup(choices=bool_list, value=bool_active, label="Boolean command-line flags", elem_classes='checkboxgroup-table')
+#             shared.gradio['reset_interface'] = gr.Button("Apply and restart the interface")
+#             with gr.Row():
+#                 extension_name = gr.Textbox(lines=1, label='Install or update an extension', info='Enter the GitHub URL below. For a list of extensions, see: https://github.com/oobabooga/text-generation-webui-extensions ⚠️  WARNING ⚠️ : extensions can execute arbitrary code. Make sure to inspect their source code before activating them.')
+#                 extension_install = gr.Button('Install or update', elem_classes="small-button")
 
-            extension_status = gr.Markdown()
+#             extension_status = gr.Markdown()
 
-            extension_install.click(
-                clone_or_pull_repository, extension_name, extension_status, show_progress=False).then(
-                lambda: gr.update(choices=utils.get_available_extensions(), value=shared.args.extensions), outputs=shared.gradio['extensions_menu'])
+#             extension_install.click(
+#                 clone_or_pull_repository, extension_name, extension_status, show_progress=False).then(
+#                 lambda: gr.update(choices=utils.get_available_extensions(), value=shared.args.extensions), outputs=shared.gradio['extensions_menu'])
 
-            # Reset interface event
-            shared.gradio['reset_interface'].click(
-                set_interface_arguments, [shared.gradio[k] for k in ['interface_modes_menu', 'extensions_menu', 'bool_menu']], None).then(
-                lambda: None, None, None, _js='() => {document.body.innerHTML=\'<h1 style="font-family:monospace;margin-top:20%;color:lightgray;text-align:center;">Reloading...</h1>\'; setTimeout(function(){location.reload()},2500); return []}')
+#             # Reset interface event
+#             shared.gradio['reset_interface'].click(
+#                 set_interface_arguments, [shared.gradio[k] for k in ['interface_modes_menu', 'extensions_menu', 'bool_menu']], None).then(
+#                 lambda: None, None, None, _js='() => {document.body.innerHTML=\'<h1 style="font-family:monospace;margin-top:20%;color:lightgray;text-align:center;">Reloading...</h1>\'; setTimeout(function(){location.reload()},2500); return []}')
 
-            shared.gradio['toggle_dark_mode'].click(lambda: None, None, None, _js='() => {document.getElementsByTagName("body")[0].classList.toggle("dark")}')
+#             shared.gradio['toggle_dark_mode'].click(lambda: None, None, None, _js='() => {document.getElementsByTagName("body")[0].classList.toggle("dark")}')
 
        
         # chat mode event handlers
